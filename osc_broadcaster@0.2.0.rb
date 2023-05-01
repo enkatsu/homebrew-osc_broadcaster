18c6e15614efcf206a67736316e9bf5a10cb39da
Katsuya Endoh <studio@enkatsu.org>
2023-05-01 21:29:45 +0900

    upgrade

--- osc_broadcaster@0.2.0.rb
+++ osc_broadcaster@0.2.0.rb
@@ -1,7 +1,7 @@
 class OscBroadcaster < Formula
   desc "Open Sound Control broadcast server that can be started from the command-line"
   homepage "https://osc-broadcaster.enkatsu.org/"
-  url "https://github.com/enkatsu/osc_broadcaster.git", tag: "v0.2.1", revision: "f21e0049fe3f98f1944169ce7d283677c42d29d9"
+  url "https://github.com/enkatsu/osc_broadcaster.git", tag: "v0.2.0", revision: "f25ad4c7add8f55e1f67ff8b2421839130bdd197"
   license "MIT"
   depends_on "rust" => :build
 
